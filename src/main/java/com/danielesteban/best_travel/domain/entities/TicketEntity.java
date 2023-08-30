85eac686fb8d56a541676d3a008ac96ab4318253
Daniel Esteban <decepeda@misena.edu.co>
2023-08-29 20:01:22 -0500

    Se crean las entidades de CustomerEntity, ReservationEntity y TicketEntity

---
+++ src/main/java/com/danielesteban/best_travel/domain/entities/TicketEntity.java
@@ -0,0 +1,26 @@
+package com.danielesteban.best_travel.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity(name = "ticket")
+@NoArgsConstructor @AllArgsConstructor
+@Data @Builder
+public class TicketEntity implements Serializable {
+
+    @Id
+    private UUID id;
+    private LocalDate departureDate;
+    private LocalDate arrivalDate;
+    private LocalDate purchaseDate;
+    private BigDecimal price;
+}
