e9cee1a8df124372b06239ba97f3995c42ea16a5
Daniel Esteban <decepeda@misena.edu.co>
2023-08-31 19:29:27 -0500

    Probamos los repositorios

--- src/main/java/com/danielesteban/best_travel/BestTravelApplication.java
+++ src/main/java/com/danielesteban/best_travel/BestTravelApplication.java
@@ -1,11 +1,12 @@
 package com.danielesteban.best_travel;
 
-import com.danielesteban.best_travel.domain.repositories.FlyRepository;
-import com.danielesteban.best_travel.domain.repositories.HotelRepository;
+import com.danielesteban.best_travel.domain.repositories.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 @Slf4j
@@ -13,10 +14,21 @@ public class BestTravelApplication implements CommandLineRunner {
 
     private final HotelRepository hotelRepository;
     private final FlyRepository flyRepository;
+    private final TicketRepository ticketRepository;
+    private final ReservationRepository reservationRepository;
+    private final TourRepository tourRepository;
+    private final CustomerRepository customerRepository;
 
-    public BestTravelApplication(HotelRepository hotelRepository, FlyRepository flyRepository) {
+
+    public BestTravelApplication(HotelRepository hotelRepository, FlyRepository flyRepository,
+                                 TicketRepository ticketRepository, ReservationRepository reservationRepository,
+                                 TourRepository tourRepository, CustomerRepository customerRepository) {
         this.hotelRepository = hotelRepository;
         this.flyRepository = flyRepository;
+        this.ticketRepository = ticketRepository;
+        this.reservationRepository = reservationRepository;
+        this.tourRepository = tourRepository;
+        this.customerRepository = customerRepository;
     }
 
     public static void main(String[] args) {
@@ -27,8 +39,14 @@ public class BestTravelApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         var fly = flyRepository.findById(14L).get();
         var hotel = hotelRepository.findById(7L).get();
+        var ticket = ticketRepository.findById(UUID.fromString("12345678-1234-5678-2236-567812345678")).get();
+        var reservation = reservationRepository.findById(UUID.fromString("32345678-1234-5678-1234-567812345678")).get();
+        var customer = customerRepository.findById("BBMB771012HMCRR022").get();
 
         log.info(String.valueOf(fly));
         log.info(String.valueOf(hotel));
+        log.info(String.valueOf(ticket));
+        log.info(String.valueOf(reservation));
+        log.info(String.valueOf(customer));
     }
 }
