917c82a957375bda0afda68e943fdb2412600b47
Daniel Esteban <decepeda@misena.edu.co>
2023-09-26 17:08:11 -0500

    Se cambia el tipo de dato del departure y el arrivalDate

--- src/main/java/com/danielesteban/best_travel/domain/entities/TicketEntity.java
+++ src/main/java/com/danielesteban/best_travel/domain/entities/TicketEntity.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "ticket")
@@ -21,8 +22,8 @@ public class TicketEntity implements Serializable {
 
     @Id
     private UUID id;
-    private LocalDate departureDate;
-    private LocalDate arrivalDate;
+    private LocalDateTime departureDate;
+    private LocalDateTime arrivalDate;
     private LocalDate purchaseDate;
     private BigDecimal price;
     @ManyToOne @JoinColumn(name = "fly_id")
