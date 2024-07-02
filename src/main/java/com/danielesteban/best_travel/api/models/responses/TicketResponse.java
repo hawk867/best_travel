d39f545a0ac8e2ef7ea4565048339b1f6b8af2ea
Daniel Esteban <decepeda@misena.edu.co>
2024-07-01 20:03:50 -0500

    Dando formato a las fechas con jackson databind

--- src/main/java/com/danielesteban/best_travel/api/models/responses/TicketResponse.java
+++ src/main/java/com/danielesteban/best_travel/api/models/responses/TicketResponse.java
@@ -1,5 +1,6 @@
 package com.danielesteban.best_travel.api.models.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +17,11 @@ import java.util.UUID;
 public class TicketResponse implements Serializable {
 
     private UUID id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH-mm")
     private LocalDateTime departureDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH-mm")
     private LocalDateTime arrivalDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate purchaseDate;
     private BigDecimal price;
     private FlyResponse fly;
